35a5fcbab71f05b89bdd871748d0f9e04264163b
om0201 <74784928+om0201@users.noreply.github.com>
2020-12-15 16:29:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>The tastes of INDIA</h1>
+      <span>
+        <button>East</button>
+        <button>West</button>
+        <button>North</button>
+        <button>South</button>
+      </span>  
+    </div>
+  );
+}
